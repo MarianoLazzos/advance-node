df71e7d77d91ec8397f23ea3622c8b16d9a87b3c
MarianoLazzos <lazzos97@gmail.com>
2022-05-17 08:33:50 -0300

    advance node course

---
+++ AdvancedNodeStarter/config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '794053760200-2esru8dfhnptdbcuasqm0reg006b19nd.apps.googleusercontent.com',
+  googleClientSecret: 'sD5G0kp7sHoIuC5wHtQb8Xhw',
+  mongoURI: 'mongodb://marianolazzos:Marianodl894@advancenode-shard-00-00.agfsr.mongodb.net:27017,advancenode-shard-00-01.agfsr.mongodb.net:27017,advancenode-shard-00-02.agfsr.mongodb.net:27017/advancenode?ssl=true&replicaSet=atlas-j5j5pt-shard-0&authSource=admin&retryWrites=true&w=majority',
+  cookieKey: '123123123',
+};
